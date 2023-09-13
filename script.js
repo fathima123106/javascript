d3d986c14c26677bb17a16e3ab906c3248ae5ba6
fathi <binufathi006@gmail.com>
2023-09-13 09:14:53 +0530

    updated

--- script.js
+++ script.js
@@ -1,65 +1,62 @@
-let strr="  daddy mom ,mom";
+let strr = "  daddy mom ,mom";
 let str_new = strr;
 console.log("string length : ", str_new);
-let sliced_strng=strr.slice(1,3)
-console.log("string length : ",sliced_strng);
-let sliced_strng1=strr.slice(1)
-console.log("string length : ",sliced_strng1);
-let sliced_strng2=strr.slice(-1)
-console.log("string length : ",sliced_strng2);
-let sliced_strng3=strr.slice(-3)
-console.log("string length : ",sliced_strng3);
+let sliced_strng = strr.slice(1, 3);
+console.log("string length : ", sliced_strng);
+let sliced_strng1 = strr.slice(1);
+console.log("string length : ", sliced_strng1);
+let sliced_strng2 = strr.slice(-1);
+console.log("string length : ", sliced_strng2);
+let sliced_strng3 = strr.slice(-3);
+console.log("string length : ", sliced_strng3);
 
- let substring1=strr.substring(2)
- console.log("sub string : ",substring1);
- let substring2=strr.substring(1,4);
- console.log("sub string : ",substring2);
- let substring3=strr.substring(-2)
- console.log("sub string : ",substring3);
+let substring1 = strr.substring(2);
+console.log("sub string : ", substring1);
+let substring2 = strr.substring(1, 4);
+console.log("sub string : ", substring2);
+let substring3 = strr.substring(-2);
+console.log("sub string : ", substring3);
 
- let string_replace=strr.replace("mom","mommy");
- console.log("replaced atring :" ,string_replace);
- 
- let string_concat=strr.concat(",baby");
- console.log("concatinate string : ",string_concat);
+let string_replace = strr.replace("mom", "mommy");
+console.log("replaced atring :", string_replace);
 
- let string_replaceAlll=strr.replaceAll("mom","mommy");
- console.log("replaced atring :" ,string_replaceAlll);
+let string_concat = strr.concat(",baby");
+console.log("concatinate string : ", string_concat);
 
- let trim = strr.trim();
- console.log("trimed variable :",trim)
-  let trim1= strr.trimEnd()
-  console.log("temed:",trim1);
-   let charat= strr.charAt(3);
-   console.log(charat)
+let string_replaceAlll = strr.replaceAll("mom", "mommy");
+console.log("replaced atring :", string_replaceAlll);
 
-   let charcode= strr.charCodeAt(4);
-   console.log("char at :",charcode);
+let trim = strr.trim();
+console.log("trimed variable :", trim);
+let trim1 = strr.trimEnd();
+console.log("temed:", trim1);
+let charat = strr.charAt(3);
+console.log(charat);
 
-   let indexof=strr.indexOf("m")
-   console.log("indexof",indexof);
+let charcode = strr.charCodeAt(4);
+console.log("char at :", charcode);
 
+let indexof = strr.indexOf("m");
+console.log("indexof", indexof);
 
-   let indexOf=strr.indexOf("m",9)
-   console.log("indexof m after 9 th position is :",indexOf);
+let indexOf = strr.indexOf("m", 9);
+console.log("indexof m after 9 th position is :", indexOf);
 
-let sts= strr.startsWith("hello");
-console.log("starts with :",sts);
- 
-let end_with=strr.endsWith("m");
-console.log("end with",end_with);
+let sts = strr.startsWith("hello");
+console.log("starts with :", sts);
 
-let splits=strr.split();
-console.log(splits)
+let end_with = strr.endsWith("m");
+console.log("end with", end_with);
 
-let splits1=strr.split(",").join(",");
-console.log(splits1)
+let splits = strr.split();
+console.log(splits);
+
+let splits1 = strr.split(",").join(",");
+console.log(splits1);
 
 //changed
 
-let a=1
-let b=2
-let subtraction= b-a
-console.log(subtraction)
-
-
+// let a=1
+// let b=2
+// let subtraction= b-a
+// console.log(subtraction)
---
+++ script2.js
@@ -0,0 +1,129 @@
+/*-----------------ARRAY--------------------*/
+const arr = ["apple", "orange", "grape"];
+console.log(arr);
+const fruit = new Array("plum", "mango", "cherry");
+console.log(fruit);
+
+//ARRAY LENGTH
+console.log("array length", arr.length);
+console.log("joined array: ", arr.join(","));
+console.log(arr[1]);
+console.log(fruit[2]);
+console.log("index value of apple is :", arr.indexOf("apple"));
+//to add new item to an array
+arr.push("water melon");
+console.log(arr);
+arr.pop("water melon");
+console.log(arr);
+arr.pop();
+console.log(arr);
+//to delet first element
+arr.shift();
+console.log(arr);
+//to delete or add elements into an array within index
+fruit.splice(0, 1);
+console.log(fruit);
+
+
+
+
+
+
+
+/*-----------------------OBJECTS----------------------- */
+
+const obj = {
+  car: "sumo",
+  owner: "ramu",
+  age: 25,
+};
+console.log("new object : ", obj);
+
+console.log(obj.age);
+console.log(obj.car);
+console.log(obj.owner);
+console.log(JSON.stringify(obj));
+console.log(JSON.parse(JSON.stringify(obj)));
+
+const arr2 = [
+  {
+    car: "sumo",
+    owner: "ramu",
+    age: 25,
+  },
+  {
+    car: "tata",
+    owner: "manu",
+    age: 24,
+  },
+  {
+    car: "ford",
+    owner: "Anu",
+    age: 20,
+  },
+];
+console.log(arr2);
+
+obj.owner = "fathima";
+console.log(obj);
+
+arr2[1].owner = "mohan";
+console.log(arr2);
+
+console.log(JSON.stringify(arr2));
+console.log(JSON.parse(JSON.stringify(arr2)));
+
+
+
+
+
+
+
+
+/*-------------CONDITIONS------------- */
+
+let i = 2;
+
+if (i >= 3) {
+  console.log("greater than 3");
+} else {
+  console.log("less than 3");
+}
+
+if (i <= 0) {
+  console.log("less than 0");
+} else {
+  if (i == 0) {
+    console.log("its a zero");
+  } else {
+    console.log("greater than zero");
+  }
+}
+
+let j=5
+if(j<0){
+    console.log("lesser than 0");
+}else if(j>0){
+    console.log("greater than 0")
+}else{
+    console.log("zero")
+}
+
+
+
+
+/*----------------SWITCH---------------*/
+
+
+
+console.log(i)
+
+switch(i){
+    case 1: console.log("one");
+            break;
+    case 2: console.log("two");
+            break;
+    case 3: console.log("three");
+            break;
+    default: console.log("something else")
+}
