2dded672041891d27999e6cfa08500825b07d986
nihal <ashishkshaji2002@gmail.com>
2023-09-16 09:13:56 +0530

    new

--- script3.js
+++ script3.js
@@ -133,3 +133,42 @@ function myFunctionOuter(){
  console.log("myFunction :",myfuctioncall)
  console.log("myFunctionOuter call :",myfuctioncall());
  console.log("final call :",myfuctioncall()())
+
+
+
+ /*-------------------SET INTERVEL------------------------ */
+
+//  setInterval(function my(){
+//     console.log("*")
+//  }),(1000)
+
+// setTimeout(function my(){
+//     alert("5 seconds later")
+// },5000)
+
+/*---------------------------------12hr format time--------------------------- */
+
+function putZero(sec){
+   return sec<10?"0"+sec:sec; 
+}
+
+function show(){
+    var time=document.getElementById("time")
+    var dt=new Date();
+    var hours=dt.getHours();
+    var ampm=hours>12?"pm":"am";
+    time.innerHTML=putZero(hours%12)+":"+putZero(dt.getMinutes())+":"+putZero(dt.getSeconds())+ampm;
+    setTimeout(()=>{
+        show();
+
+    },1000);
+}
+show();
+
+
+/* callback function= a function is its self a parameter of another  function
+anonymus fuction= function without name eg:- function(){
+    code-block
+}  
+arrow function eg:- =>
+*/
