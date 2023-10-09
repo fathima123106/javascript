bfc7329206db819049def51fb952d1b6f3875768
fathima123106 <70929526+fathima123106@users.noreply.github.com>
2023-10-09 13:47:09 +0530

    Update script10_regular_ecp.js

--- script10_regular_ecp.js
+++ script10_regular_ecp.js
@@ -25,7 +25,40 @@
 //const regex=/L{2}o$/i //it will check is o is ending after exactly 2 ls helllo it also true bcs before o there is 2 ls
 
 //const regex=/L{2,4}o$/i // it checks is o comes only after exactly 2 to 4 ls 
+/*------------------regular expressions--------------------*/
 
+// const regex=/Cat/i
+//const regex=/[cr]at/
+//const regex=/[a-z 0-9]at/i
+
+//i used for check avoid using case sensitive like  cat and Cat both are different in js by using i we can make them  same and [cr]at it will chech aither cat or rat and resturn the result  and also the same way a-z and 0-9  are ranges 
+
+// const regex=/^hi/  //^ this symbol check whether the sthring starst with hi or not
+
+// const regex=/hi$/  //$ it checks whether the string ends with hi or not 
+
+// const regex=/hi$/m  // in this  m is check in multiple line otherwise it will consider as a single line 
+
+
+// const regex=/hie?/ //? it will check whether hi or hie is present in the string it will look only the word infront of it  ie /hief?/ here this will check whether hie or hief is presnt or not
+
+ // this will check whether hi or hies were present in  the string
+
+ // const regex=/wat*/ //it will check either wa or wat or wat[a-z] is present in the string
+
+// const regex=/wat+/ //it will check the word wat is present atleast one or not
+
+const regex=/hi.$/ //it chech whether 
+
+let str1="that cat eat a  wa meat  hi"
+let str2="hi emmy"
+let str3=`hi     
+my name is meera
+what is your name
+i am going
+`  //backtick`
+const res=regex.test(str1);
+console.log("result :",res)
 
 //const regex=/heL{2}o$/i //it checks o comes after he and 2 ls other wise false  
 
