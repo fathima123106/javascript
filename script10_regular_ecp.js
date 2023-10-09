4ef0ac5bef12963da598bf8a5d1558488d1cb5a0
fathima123106 <70929526+fathima123106@users.noreply.github.com>
2023-10-09 13:46:51 +0530

    Update script10_regular_ecp.js

--- script10_regular_ecp.js
+++ script10_regular_ecp.js
@@ -21,14 +21,33 @@
 
 // const regex=/wat+/ //it will check the word wat is present atleast one or not
 
-const regex=/hi.$/ //it chech whether 
+// 
+//const regex=/L{2}o$/i //it will check is o is ending after exactly 2 ls helllo it also true bcs before o there is 2 ls
+
+//const regex=/L{2,4}o$/i // it checks is o comes only after exactly 2 to 4 ls 
+
+
+//const regex=/heL{2}o$/i //it checks o comes after he and 2 ls other wise false  
+
+//const regex=/heL{2,4}o$/i //it checks o comes after he and 2 to 4 ls other wise false 
+
+//const regex=/heL{2,}o$/i //it checks o comes after he and minimum 2 ls and maximum infinite other wise false like hellllllllllllllllllllllllllllllllo it will be true and helo will be false
+
+/*----------------------------- \-escape character--------------------------------*/
+
+
+//const regex=/\d/i //if any digits are present or not
+
+//const regex=/\D/i //if any single non-digits are present or not if present it return ture otherwise false
+
+const regex=/\./ // /./ simply it returns true whatever comes in the string is we want to check whether . comes in a string we have to use/\./ it returns true only if . comes in a string same as \$ to check whwtwehr $ comes in a string or not
 
 let str1="that cat eat a  wa meat  hi"
-let str2="hi emmy"
+let str2="hi emmy hello"
 let str3=`hi     
 my name is meera
 what is your name
 i am going
 `  //backtick`
-const res=regex.test(str1);
+const res=regex.test(str2);
 console.log("result :",res)
